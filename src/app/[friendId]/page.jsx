4b863a07ce3eb33d3d69768bcd198b96dffaac7d
thisisyourfahi <fahiyanofficial@gmail.com>
2026-05-05 11:04:23 +0600

    style: responsive friend detail page

--- src/app/[friendId]/page.jsx
+++ src/app/[friendId]/page.jsx
@@ -12,15 +12,15 @@ const FriendProfile = async ({ params }) => {
     const friend = friends.find(friend => friend.id == friendId);
 
     return (
-        <div className='container mx-auto'>
-            <div className='flex md:flex-row md:gap-10 mt-20 mb-20 items-center'>
+        <div className='container mx-auto p-4 md:p-0'>
+            <div className='flex flex-col md:flex-row md:gap-10 mt-4 md:mt-20 mb-20 items-center'>
                 {/* left */}
                 <div className='md:w-[30%]'>
                     <ProfileLeft friend={friend} />
                 </div>
 
                 {/* right */}
-                <div className='md:w-[70%] p-20'>
+                <div className='md:w-[70%] mt-4 md:mt-0'>
                     <ProfileRight friend={friend}/>
                 </div>
             </div>
